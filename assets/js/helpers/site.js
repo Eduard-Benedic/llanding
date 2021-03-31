8972940e57a1cab5bc09190366a511fd182492f9
Eduard <benediceduard@outlook.com>
2021-03-31 13:37:28 +0100

    backup page

--- assets/js/helpers/site.js
+++ assets/js/helpers/site.js
@@ -1,40 +1,43 @@
 
-const select = (selector) => {
-    return document.querySelectorAll(selector)
-}
+// const select = (selector, single) => {
+//     if (!single)
+//         return document.querySelectorAll(selector)
+//     return document.querySelector(selector)
+// }
 
-const pythagora = (xPair) => {
+// const pythagora = (xPair) => {
 
-}
+// }
 
-const getCoordinates = (element) => {
-    const coordinates = element.getBoundingClientRect();
-    console.log(coordinates)
-    return coordinates;
-};
-
-const setStartLineAttribute = (el, coordinates) => {
-    el.setAttribute('x1', coordinates.x + 'px');
-    el.setAttribute('y1', coordinates.y + 'px');
-}
-
-const setEndLineAttribute = (el, coordinates) => {
-    el.setAttribute('x2', coordinates.x + 'px');
-    el.setAttribute('y2', coordinates.y + 'px');
-}
+// const getCoordinates = (element) => {
+//     const coordinates = element.getBoundingClientRect();
+//     return coordinates;
+// };
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const roadline = select('#roadline')[0]
-    const roadReferenceEl = select('[el="road-ref"]')
-    const start = getCoordinates(roadReferenceEl[0])
-    const end = getCoordinates(roadReferenceEl[1])
+// const selectors = {
+//     roadEl: '[el="road-path"]'
+// }
 
-    setStartLineAttribute(roadline, start)
-    setEndLineAttribute(roadline, end)
+// document.addEventListener('DOMContentLoaded', () => {
+//     const roadline = select('#roadline')[0]
+//     const roadReferenceEl = select('[el="road-ref"]')
+//     const start = getCoordinates(roadReferenceEl[0])
+//     const end = getCoordinates(roadReferenceEl[1])
 
-    // roadline.setAttribute('x3', '400px');
-    // roadline.setAttribute('y3', '900px')
-    // console.log(getCoordinates(roadReferenceEl[0]))
+
+//     const pathElement = select(selectors.roadEl, true)
+
+//     // const moveTo = {
+//     //     x: 
+//     // }
+
+//     pathElement.setAttribute('d', `M ${start.x} ${start.y} Q ${(end.x + start.x / 2)} ${(end.y + start.y) / 2} ${end.x} ${end.y}`)
+//     console.log(start, end)
+//     console.log(pathElement)
+
+//     // roadline.setAttribute('x3', '400px');
+//     // roadline.setAttribute('y3', '900px')
+//     // console.log(getCoordinates(roadReferenceEl[0]))
     
-})
+// })
