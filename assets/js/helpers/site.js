4beaa78b0bd9b30bbc19acf97597f1920795395e
Eduard <benediceduard@outlook.com>
2021-03-31 17:17:04 +0100

    omg road done

--- assets/js/helpers/site.js
+++ assets/js/helpers/site.js
@@ -1,43 +1,45 @@
 
-// const select = (selector, single) => {
-//     if (!single)
-//         return document.querySelectorAll(selector)
-//     return document.querySelector(selector)
-// }
+const select = (selector, single) => {
+    if (!single)
+        return document.querySelectorAll(selector)
+    return document.querySelector(selector)
+}
 
-// const pythagora = (xPair) => {
+const pythagora = (xPair) => {
 
-// }
+}
 
-// const getCoordinates = (element) => {
-//     const coordinates = element.getBoundingClientRect();
-//     return coordinates;
-// };
-
-
-// const selectors = {
-//     roadEl: '[el="road-path"]'
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const roadline = select('#roadline')[0]
-//     const roadReferenceEl = select('[el="road-ref"]')
-//     const start = getCoordinates(roadReferenceEl[0])
-//     const end = getCoordinates(roadReferenceEl[1])
+const getCoordinates = (element) => {
+    const coordinates = element.getBoundingClientRect()
+    return coordinates
+}
 
 
-//     const pathElement = select(selectors.roadEl, true)
+const selectors = {
+    roadEl: '[el="road-path"]',
+    roadRef: '[el="road-ref"]'
+}
 
-//     // const moveTo = {
-//     //     x: 
-//     // }
+document.addEventListener('DOMContentLoaded', () => {
+    const roadline = select('#roadline')[0]
+    const roadReferenceEl = select(selectors.roadRef)
+    const start = getCoordinates(roadReferenceEl[0])
+    const middle = getCoordinates(roadReferenceEl[1])
+    const end = getCoordinates(roadReferenceEl[2])
+    const roadMap = getCoordinates(select('.roadmap', true))
 
-//     pathElement.setAttribute('d', `M ${start.x} ${start.y} Q ${(end.x + start.x / 2)} ${(end.y + start.y) / 2} ${end.x} ${end.y}`)
-//     console.log(start, end)
-//     console.log(pathElement)
+    const pathElement = select(selectors.roadEl, true)
+   
 
-//     // roadline.setAttribute('x3', '400px');
-//     // roadline.setAttribute('y3', '900px')
-//     // console.log(getCoordinates(roadReferenceEl[0]))
+    const startDx = start.left;
+    const startDy = roadMap.top - start.top + start.height / 2;
     
-// })
+    const middleDx = middle.left;
+    const middleDy =  middle.top - roadMap.top  + middle.height / 2;
+
+    const endDx = end.left;
+    const endDy = roadMap.bottom - end.height / 2;
+
+     pathElement.setAttribute('d',
+            `M ${startDx} ${startDy} C ${startDx} ${startDy + 300} ${middleDx + 50} ${middleDy - 200} ${middleDx} ${middleDy} S 300 300 ${endDx} ${endDy}`)
+})
