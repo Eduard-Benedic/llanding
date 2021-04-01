0d3ebdc520708e761b897a95d55853026707e580
Eduard <benediceduard@outlook.com>
2021-04-01 11:04:40 +0100

    road ready to go

--- assets/js/helpers/site.js
+++ assets/js/helpers/site.js
@@ -5,26 +5,24 @@ const select = (selector, single) => {
     return document.querySelector(selector)
 }
 
-const pythagora = (xPair) => {
-
-}
-
-const getCoordinates = (element) => {
-    const coordinates = element.getBoundingClientRect()
-    return coordinates
-}
-
-
 const selectors = {
     roadEl: '[el="road-path"]',
     roadRef: '[el="road-ref"]',
     scrollTrigger: '[el="scroll-trigger"]'
 }
 
+const getCoordinates = (element) => {
+    
+    return element.getBoundingClientRect()
+}
+
+const getRelativeY = (parentCoord, elCoord) => {
+    return (parentCoord.height - elCoord.height / 2) - (parentCoord.bottom - elCoord.bottom);
+}
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
-
-  
     const roadTriggerEl = select(selectors.scrollTrigger, true)
     const roadReferenceEl = select(selectors.roadRef)
     const start = getCoordinates(roadReferenceEl[0])
@@ -33,28 +31,28 @@ window.addEventListener('DOMContentLoaded', () => {
     const roadMap = getCoordinates(select('.roadmap', true))
     const pathElement = select(selectors.roadEl, true)
 
-    
-    const controller = new ScrollMagic.Controller({ globalSceneOptions: { duration: 1000 } })
-    
 
-    new ScrollMagic.Scene({
-            triggerElement: selectors.scrollTrigger
-        })
-        .addTo(controller)
-        .on('enter leave', () => {
-            roadTriggerEl.classList.toggle('animate')
-        })
-   
-
+    
     const startDx = start.left;
-    const startDy = roadMap.top - start.top + start.height / 2;
-    
+    const startDy = getRelativeY(roadMap, start)
+
     const middleDx = middle.left;
-    const middleDy =  middle.top - roadMap.top  + middle.height / 2;
+    const middleDy = getRelativeY(roadMap, middle);
 
     const endDx = end.left;
-    const endDy = roadMap.bottom - end.height / 2;
+    const endDy = getRelativeY(roadMap, end);
 
     pathElement.setAttribute('d',
-            `M ${startDx} ${startDy} C ${startDx} ${startDy + 300} ${middleDx + 50} ${middleDy - 200} ${middleDx} ${middleDy} S 500 700 ${endDx} ${endDy + 100}`)
+            `M ${startDx} ${startDy} C ${startDx} ${startDy + 140} ${middleDx + 50} ${middleDy - 200} ${middleDx} ${middleDy} S 500 700 ${endDx} ${endDy + 100}`)
+
+
+    
+    const controller = new ScrollMagic.Controller({ globalSceneOptions: { duration: 1000 } })
+    new ScrollMagic.Scene({
+        triggerElement: selectors.scrollTrigger
+    })
+    .addTo(controller)
+    .on('enter', () => {
+        roadTriggerEl.classList.add('animate')
+    })
 })
