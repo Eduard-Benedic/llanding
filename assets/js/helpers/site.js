ec182afbd8b1b618b58f0910915c67ca50780980
Eduard <benediceduard@outlook.com>
2021-04-01 14:00:33 +0100

    toggle section done

--- assets/js/helpers/site.js
+++ assets/js/helpers/site.js
@@ -1,58 +1,113 @@
 
-const select = (selector, single) => {
+var select = function (selector, single) {
     if (!single)
         return document.querySelectorAll(selector)
     return document.querySelector(selector)
 }
 
-const selectors = {
-    roadEl: '[el="road-path"]',
-    roadRef: '[el="road-ref"]',
-    scrollTrigger: '[el="scroll-trigger"]'
+var selectors = {
+    roadPathEmployer: '[el="road-path-employer"]',
+    roadPathJobseeker: '[el="road-path-jobseeker"]',
+    roadRefEmployer: '[el="road-ref-employer"]',
+    roadRefJobseeker:  '[el="road-ref-jobseeker"]',
+    scrollTrigger: '[el="scroll-trigger"]',
+    shiftController: '[el="shiftControllerBtn"]'
 }
 
-const getCoordinates = (element) => {
-    
+var getCoordinates = function (element)  {
     return element.getBoundingClientRect()
 }
 
-const getRelativeY = (parentCoord, elCoord) => {
-    return (parentCoord.height - elCoord.height / 2) - (parentCoord.bottom - elCoord.bottom);
+var getRelativeY = function (parentCoord, elCoord)  {
+    return (parentCoord.height - elCoord.height / 2) - (parentCoord.bottom - elCoord.bottom)
 }
 
+var getRoadPath = function (start, middle, end) {
+    var roadMap = getCoordinates(select('.roadmap', true))
+    return {
+        start: {
+            x: start.left,
+            y: getRelativeY(roadMap, start)
+        },
+        middle: {
+            x: middle.left,
+            y: getRelativeY(roadMap, middle)
+        },
+        end: {
+            x: end.left,
+            y: getRelativeY(roadMap, end)
+        }
+    }
+}
 
-window.addEventListener('DOMContentLoaded', () => {
-
-    const roadTriggerEl = select(selectors.scrollTrigger, true)
-    const roadReferenceEl = select(selectors.roadRef)
-    const start = getCoordinates(roadReferenceEl[0])
-    const middle = getCoordinates(roadReferenceEl[1])
-    const end = getCoordinates(roadReferenceEl[2])
-    const roadMap = getCoordinates(select('.roadmap', true))
-    const pathElement = select(selectors.roadEl, true)
-
-
+var initiateStepRoad = function (roadReference, roadPath) {
+    var start = getCoordinates(roadReference[0])
+    var middle = getCoordinates(roadReference[1])
+    var end = getCoordinates(roadReference[2])
     
-    const startDx = start.left;
-    const startDy = getRelativeY(roadMap, start)
-
-    const middleDx = middle.left;
-    const middleDy = getRelativeY(roadMap, middle);
-
-    const endDx = end.left;
-    const endDy = getRelativeY(roadMap, end);
-
+    var pathElement = select(roadPath, true)
+    var roadPathCoordinates = getRoadPath(start, middle, end)
     pathElement.setAttribute('d',
-            `M ${startDx} ${startDy} C ${startDx} ${startDy + 140} ${middleDx + 50} ${middleDy - 200} ${middleDx} ${middleDy} S 500 700 ${endDx} ${endDy + 100}`)
+            `M ${roadPathCoordinates.start.x} ${roadPathCoordinates.start.y} C ${roadPathCoordinates.start.x} ${roadPathCoordinates.start.y + 140} ${roadPathCoordinates.middle.x + 50} ${roadPathCoordinates.middle.y - 200} ${roadPathCoordinates.middle.x} ${roadPathCoordinates.middle.y} S 500 700 ${roadPathCoordinates.end.x} ${roadPathCoordinates.end.y + 100}`)
+}
+
+var toggleClass = function (el, className) {
+    if (el.classList.contains(className)) return el.classList.remove(className)
+    return el.classList.add(className)    
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    var roadReferenceEmployer = select(selectors.roadRefEmployer)
+    var roadReferenceJobseeker = select(selectors.roadRefJobseeker)
+
+    initiateStepRoad(roadReferenceEmployer, selectors.roadPathEmployer)
+    initiateStepRoad(roadReferenceJobseeker, selectors.roadPathJobseeker)
+
+    /*=================================================
+        Toggle Shift Type
+    ====================================================*/
+    var controllerBtn = select(selectors.shiftController)
+
+    controllerBtn.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            var shiftID = this.dataset.shift
+            const shiftToggle = select('[el="shift-toggle"]')
+
+            controllerBtn.forEach(function (btn) {
+                toggleClass(btn, 'trainway__btn--active')
+            })
+
+            shiftToggle.forEach(function (el) {
+                el.style.display = 'none'
+            })
+            var shiftSection = select(shiftID, true)
+            shiftSection.style.display = 'block'
+        })
+    })
 
 
+    /*=============================================================
+        Animate the Road on Scroll
+    ===============================================================*/
+    var roadTriggerEl = select(selectors.scrollTrigger, true)
+    var roadTriggerElEmployer = select('[el="scroll-trigger-employer"]', true)
+
+    var controller = new ScrollMagic.Controller({ globalSceneOptions: { duration: 1000 } })
     
-    const controller = new ScrollMagic.Controller({ globalSceneOptions: { duration: 1000 } })
     new ScrollMagic.Scene({
-        triggerElement: selectors.scrollTrigger
+        triggerElement: roadTriggerEl
     })
     .addTo(controller)
-    .on('enter', () => {
-        roadTriggerEl.classList.add('animate')
+    .on('enter leave', function () {
+        roadTriggerEl.classList.toggle('animate')
+    })
+
+    new ScrollMagic.Scene({
+        triggerElement: roadTriggerElEmployer
+    })
+    .addTo(controller)
+    .on('enter leave', function () {
+        roadTriggerElEmployer.classList.toggle('animate')
     })
 })
